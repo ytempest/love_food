e18ebef2c6a5e70ef7dea3faa23aff016836ea89
ytempest <787491096@qq.com>
2019-02-09 17:06:52 +0800

    repair date util

--- src/main/java/com/ytempest/util/DateUtils.java
+++ src/main/java/com/ytempest/util/DateUtils.java
@@ -1,5 +1,7 @@
 package com.ytempest.util;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
@@ -8,6 +10,8 @@ import java.util.Date;
  */
 public class DateUtils {
 
+    private static final SimpleDateFormat FORMAT = new SimpleDateFormat("yyyy/MM/dd");
+
     public static Date parseDate(String dateStr) {
         Date date = null;
         if (!TextUtils.isEmpty(dateStr)) {
@@ -15,4 +19,22 @@ public class DateUtils {
         }
         return date;
     }
+
+    public static Date stringToDate(String date) {
+        if (!TextUtils.isEmpty(date)) {
+            try {
+                return FORMAT.parse(date);
+            } catch (ParseException e) {
+            }
+        }
+        return null;
+    }
+
+    public static long stringToLong(String date) {
+        if (!TextUtils.isEmpty(date)) {
+            return stringToDate(date).getTime();
+        }
+        return -1;
+    }
+
 }
